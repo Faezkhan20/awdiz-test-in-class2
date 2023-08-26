a6b7f8b189f61629319eff2ebed5e4d253940036
Faezkhan20 <138295726+Faezkhan20@users.noreply.github.com>
2023-08-26 18:27:09 +0530

    update

---
+++ test/test in class/first.js
@@ -0,0 +1,49 @@
+
+question 2 
+
+
+var array = [1,2,3,4,6,7]
+
+for(var i =0;i<array.length;i++){
+    if(array[i]%2==0){
+        console.log(array[i])
+
+    }
+}
+
+
+question number 3 
+
+function sabsechota(array){
+    let sabsechota=array[0];
+    for(var i =1; i< array.length;i++){
+        if(array[i] < sabsechota){
+    }
+}
+    return sabsechota
+    
+}
+console.log(sabsechota([100,222,333,444]))
+
+
+
+question 1 \
+
+function getlengths(array){
+    var lengths=[];
+    for(var i =0;i<array.length;i++){
+lengths.push(array[i].length)
+    }
+    return lengths
+}
+ console.log(getlengths(["faezkhan111"]))
+
+
+question 5 
+
+
+
+
+
+
+
